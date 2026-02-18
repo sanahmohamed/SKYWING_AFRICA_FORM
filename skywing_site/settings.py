ebb241ac3dbdac4d865780493f805fff60654cb4
Mohamed Sanah <sanahthottungal@gmail.com>
2026-02-19 00:19:41 +0400

    asdasd

--- skywing_site/settings.py
+++ skywing_site/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-# import dj_database_url
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,31 +75,31 @@ WSGI_APPLICATION = 'skywing_site.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+if os.environ.get('DATABASE_URL'):
+    # Production - PostgreSQL
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
-
-
-# if os.environ.get('DATABASE_URL'):
-#     # Production - PostgreSQL
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default=os.environ.get('DATABASE_URL'),
-#             conn_max_age=600,
-#             conn_health_checks=True,
-#         )
-#     }
-# else:
-#     # Local - SQLite
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+else:
+    # Local - SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ============================================================
 # WINDOWS AUTHENTICATION (alternative — no username/password)
